4a2f2eabe969ae70d1757240ae98ce7c5f196a11
van20persies <44658289+van20persies@users.noreply.github.com>
2022-03-12 10:37:22 +0000

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,29 +8,34 @@ int main(void)
 {
 	int i = '0';
 	int j = '0';
+	int k = '0';
 
-	while (i <= '9')
+	while (i <= '7')
 	{
-		while (j <= '9')
+		while (j <= '8')
 		{
-			if (!(i > j || i == j))
+			while (k <= '9')
 			{
-				putchar(i);
-				putchar(j);
-				if (i == '8' && j == '9')
+				if (i < j && j < k)
 				{
-					putchar('\n');
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
 				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				k++;
 			}
+			k = '0';
 			j++;
 		}
 		j = '0';
 		i++;
 	}
+	putchar('\n');
 	return (0);
 }
