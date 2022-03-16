7da67374c12783579c2c607036b5541f773cd9e3
van20persies <44658289+van20persies@users.noreply.github.com>
2022-03-16 10:06:11 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
 #include "main.h"
-/**
+/*
  * main - prints the string "Holberton" from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {	
-char c[] = "_putchar\n";
-int i;
-for (i = 0; i < 9; i++)
-{
-_putchar(c[i]);
-}
-return (0);
+  char c[] = "_putchar\n";
+  int i;
+  for (i = 0; i < 9; i++)
+  {
+  _putchar(c[i]);
+  }
+  return (0);
 }
