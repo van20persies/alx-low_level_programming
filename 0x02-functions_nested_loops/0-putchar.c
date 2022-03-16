7bf3d043de0e75d7694dd18162517747449c40e1
van20persies <44658289+van20persies@users.noreply.github.com>
2022-03-16 10:08:43 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {	
-  char c[] = "_putchar\n";
-  int i;
-  for (i = 0; i < 9; i++)
-  {
-  _putchar(c[i]);
-  }
-  return (0);
+char c[] = "_putchar\n";
+int i;
+ for (i = 0; i < 9; i++)
+ {
+ _putchar(c[i]);
+ }
+ return (0);
 }
