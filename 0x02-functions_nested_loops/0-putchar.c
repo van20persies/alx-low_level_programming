e6964fb9674321ea9353eedeb3e4be601d6380be
van20persies <44658289+van20persies@users.noreply.github.com>
2022-03-16 10:45:11 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * *  * main -  prints _putchar, followed by a new line
- * *   * Return: Always 0 (Success)
- * */
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ * /
 
 int main(void)
 {
-		char word[9] = "_putchar\n";
-			int i;
-
-				for (i = 0; i < 9; i++)
-						{
-									_putchar(word[i]);
-										}
-
-					return (0);
+char word[9] = "_putchar\n";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(word[i]);
+}
+	return (0);
 }
