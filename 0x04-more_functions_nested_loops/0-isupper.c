424577aba99c046052bc64c1a2e7f375e2d840be
van20persies <44658289+van20persies@users.noreply.github.com>
2022-03-17 20:50:17 +0000

    Add files via upload

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *  * _isupper -  checks for uppercase character.
+ *
+ *   * @c: c -  Variable
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int _isupper(int c)
+
+{
+
+
+
+		if (c >= 65 && c <= 90)
+
+				{
+
+							return (1);
+
+								}
+
+			return (0);
+
+}
