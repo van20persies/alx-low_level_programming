b2e630ce4d34cc55d1aa95825b8b34aa00313276
van20persies <44658289+van20persies@users.noreply.github.com>
2022-03-21 19:31:17 +0000

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcpy - function with two arguments
  * @dest: 1st argument char type pointer
@@ -9,11 +8,9 @@
  * Return: na
  */
 char *_strcpy(char *dest, char *src)
-{
-int i;
+{int i;
 for (i = 0; src[i] != '\0'; i++)
 dest[i] = src[i];
 for ( ; i < src[i]; i++)
 dest[i] = '\0';
-return (dest);	
-}
+return (dest);}
