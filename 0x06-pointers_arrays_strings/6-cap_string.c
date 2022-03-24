e2da9642a4ceb743af12136d0ea7b93a78600903
van20persies <44658289+van20persies@users.noreply.github.com>
2022-03-24 22:06:12 +0000

    Add files via upload

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*  * cap_string - capitalizes every word of a string
+*   * @s: string to modify
+*    *
+*     * Return: the resulting string
+*/
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
